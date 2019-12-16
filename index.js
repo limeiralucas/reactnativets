cf8fc24c8d157ab3b90a8c80d601b9cd0b0bc93c
Lucas Limeira <lucasalveslm@gmail.com>
2019-12-16 13:25:00 -0300

    addeed react native navigation

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import App from './src';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('navigation.playground.WelcomeScreen', () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
